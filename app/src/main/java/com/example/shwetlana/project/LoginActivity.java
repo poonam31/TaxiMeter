3eae3480ab5ec9b03ffe8feac4a6f91d7b165088
poonam31 <poonam.lakhangaonkar@gmail.com>
2016-06-01 21:30:50 -0700

    Login Request, Register Request added (database)

--- app/src/main/java/com/example/shwetlana/project/LoginActivity.java
+++ app/src/main/java/com/example/shwetlana/project/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.shwetlana.project;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,10 +32,50 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
 
-                //chek if username n pwd is admin
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
 
-                Intent homeMapIntent = new Intent(LoginActivity.this, HomeMapsActivity.class);
-                LoginActivity.this.startActivity(homeMapIntent);
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                String name =jsonResponse.getString("name");
+                                int age = jsonResponse.getInt("age");
+
+                                Intent homeMapIntent = new Intent(LoginActivity.this, HomeMapsActivity.class);
+                                LoginActivity.this.startActivity(homeMapIntent);
+
+//                                Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+//                                intent.putExtra("name", name);
+//                                intent.putExtra("username", username);
+//                                intent.putExtra("age", age);
+//
+//                                LoginActivity.this.startActivity(intent);
+                            }
+                            else{
+                                AlertDialog.Builder builder = new  AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
 
             }
 
--- app/src/main/java/com/example/shwetlana/project/UserAreaActivity.java
+++ app/src/main/java/com/example/shwetlana/project/UserAreaActivity.java
@@ -1,5 +1,6 @@
 package com.example.shwetlana.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -15,5 +16,15 @@ public class UserAreaActivity extends AppCompatActivity {
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", -1);
+
+        String message= name + "welcome to app";
+        tvWelcomeMsg.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
     }
 }
